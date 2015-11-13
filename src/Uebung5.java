0ac55eeaa2f39310229d021ccdf431878ba480f6
Philipp <philipp.kropik@gmail.com>
2015-11-13 16:22:12 +0100

    Um "Erklärung" ergänzt

--- src/Uebung5.java
+++ src/Uebung5.java
@@ -4,32 +4,33 @@ public class Uebung5 {
 	public static void main(String[] args) {
 		int[] zahlen1 = {1,5,8,9,12,33,17,25,14,11};
 		
-		int ergebniss =0;
-		for (int a=0;a < zahlen1.length; a++)
+		int ergebniss =0; // Ergebniss auf 0 setzen. 
+		for (int a=0;a < zahlen1.length; a++) // Schleifensteuerung. Variable / Exitbedingung/ Modifikator je Run 
 		{
-			ergebniss = ergebniss + zahlen1[a];
+			ergebniss = ergebniss + zahlen1[a]; //Alle im Array enthaltenen Zahlen zusammenzählen und in die Ergebnissvariable speichern.  
 		}
-		System.out.println(ergebniss);
-	
+		System.out.println(ergebniss); // Ergebniss ausgeben. 
+	//zu suchende Zahl definieren 
 	int vergleichsZahl=6;
-
+	// Ergebniss Variable setzen und variablen an das Unterprogramm übergeben
 	boolean nEnthalten = check(zahlen1,vergleichsZahl);
 	
 	System.out.println(nEnthalten);
 	}
-	public static boolean check(int[] zahlen1, int n)
+	public static boolean check(int[] zahlen1, int n) //Prüfprogramm für den Vergleich 
 	{
-		for (int b = 0; b < zahlen1.length; b++ )
+		for (int b = 0; b < zahlen1.length; b++ ) // Beginn der Schleife
 		{
-			if (zahlen1[b] == n)
+			if (zahlen1[b] == n) // Vergleich der Aktuellen Zahl im Array mit der gesuchten Zahl. 
 			{
-			return true;
+				return true; // Erfolgreich, Methode wird mit Erfolg beendet 
 			}
 			else 
 			{
-				continue;
+				continue; // Once more from the Top
 			}
-		}
-		return false;
+		} // Ende der Schleife 
+		
+		return false; // Fehlschlag die Zahl ist nicht enthalten und der lauf wird beendet. 
 	}
 }
