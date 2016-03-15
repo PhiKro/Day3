0ce29583ea2dbab46f0b405642fe407f474475ac
Philipp <philipp.kropik@gmail.com>
2016-03-15 18:57:13 +0100

    k

---
+++ src/Uebung6.java
@@ -0,0 +1,26 @@
+
+public class Uebung6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int [][] a = new int [2][3];
+	a[0][0]=1;
+	a[0][1]=2;
+	a[0][2]=1;
+	a[1][0]=2;
+	a[1][1]=3;
+	a[1][2]=6;
+	
+	for (int i =0;i<a.length; i++)
+	{
+		for (int j=0;j<a[i].length;j++)
+		{
+			System.out.print(a[i][j]+",");
+		}
+	System.out.println("");
+	}
+	
+	
+	}
+
+}
